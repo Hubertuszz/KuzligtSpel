f07417c213e3fa9bcd9af95504bd16e25b8be297
SchlyfeMormor <44261512+SchlyfeMormor@users.noreply.github.com>
2019-09-30 15:22:26 +0200

    allt utom kamera

---
+++ Kuzligt Spel/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Public Fields
+
+    public static GameManager Instance;
+
+    #endregion
+
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Kuzligt Spel/Assets/Scripts/PlayerManager.cs
+++ Kuzligt Spel/Assets/Scripts/PlayerManager.cs
@@ -2,14 +2,22 @@
 using UnityEngine.EventSystems;
 
 using System.Collections;
-
+using Photon.Pun;
 
 /// <summary>
 /// Player manager.
 /// Handles fire Input and Beams.
 /// </summary>
-public class PlayerManager : MonoBehaviour
+public class PlayerManager : MonoBehaviourPunCallbacks
 {
+
+    #region Public Fields
+
+    [Tooltip("The current Health of our player")]
+    public float Health = 1f;
+
+    #endregion
+
     #region Private Fields
 
     [Tooltip("The Beams GameObject to control")]
@@ -17,6 +25,7 @@ public class PlayerManager : MonoBehaviour
     private GameObject beams;
     //True, when the user is firing
     bool IsFiring;
+
     #endregion
 
     #region MonoBehaviour CallBacks
@@ -46,6 +55,10 @@ public class PlayerManager : MonoBehaviour
         if (photonView.IsMine)
         {
             ProcessInputs();
+            if (Health <= 0f)
+            {
+                GameManager.Instance.LeaveRoom();
+            }
         }
 
         // trigger Beams active state
@@ -55,6 +68,47 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// MonoBehaviour method called when the Collider 'other' enters the trigger.
+    /// Affect Health of the Player if the collider is a beam
+    /// Note: when jumping and firing at the same, you'll find that the player's own beam intersects with itself
+    /// One could move the collider further away to prevent this or check if the beam belongs to the player.
+    /// </summary>
+    void OnTriggerEnter(Collider other)
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+        // We are only interested in Beamers
+        // we should be using tags but for the sake of distribution, let's simply check by name.
+        if (!other.name.Contains("Beam"))
+        {
+            return;
+        }
+        Health -= 0.1f;
+    }
+    /// <summary>
+    /// MonoBehaviour method called once per frame for every Collider 'other' that is touching the trigger.
+    /// We're going to affect health while the beams are touching the player
+    /// </summary>
+    /// <param name="other">Other.</param>
+    void OnTriggerStay(Collider other)
+    {
+        // we dont' do anything if we are not the local player.
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+        // We are only interested in Beamers
+        // we should be using tags but for the sake of distribution, let's simply check by name.
+        if (!other.name.Contains("Beam"))
+        {
+            return;
+        }
+        // we slowly affect health when beam is constantly hitting us, so player has to move to prevent death.
+        Health -= 0.1f * Time.deltaTime;
+    }
     #endregion
 
     #region Custom
@@ -82,4 +136,3 @@ public class PlayerManager : MonoBehaviour
 
     #endregion
 }
-
